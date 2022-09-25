ce445eee9e57f002721c070f811fb7bf2666a47e
warifen945 <113836453+warifen945@users.noreply.github.com>
2022-09-25 17:40:57 +0200

    Add files via upload

---
+++ NSIT-main/NSIT-main/space_solution_3.py
@@ -0,0 +1,38 @@
+import pygame  # necessaire pour charger les images et les sons
+
+class Joueur() : # classe pour créer le vaisseau du joueur
+    def __init__(self) :
+        self.position = 400
+        self.image = pygame.image.load("vaisseau.png")
+        self.sens = "O"
+        self.vitesse = 5
+
+    def deplacer(self) :
+        if (self.sens == "droite") and (self.position < 740):
+            self.position = self.position + self.vitesse
+        elif (self.sens == "gauche") and (self.position > 0):
+           self.position = self.position - self.vitesse
+           
+    def tirer(self):
+        self.sens = "O"
+
+class Balle() :
+    def __init__(self, tireur):
+        self.tireur = tireur
+        self.depart = tireur.position + 16
+        self.hauteur = 492
+        self.image = pygame.image.load("balle.png")
+        self.etat = "chargee"
+    
+    def bouger(self):
+        if self.etat == "attente":
+            self.depart = self.tireur.position + 16
+            self.hauteur = 492
+        elif self.etat == "tiree" :
+            self.hauteur = self.hauteur - 5
+        
+        if self.hauteur < 0:
+            self.etat = "chargee"
+
+        
+        
